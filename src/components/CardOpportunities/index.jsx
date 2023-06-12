51be3e62b07301fde1c0f8f4492c718a924451c5
张继芮 <1528879339@qq.com>
2023-06-12 17:38:19 +0800

    招聘

--- src/components/CardOpportunities/index.jsx
+++ src/components/CardOpportunities/index.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import './inderx.scss'
-import { Modal, Row, Col } from 'antd'
+import { Modal, Row, Col, Upload, message } from 'antd'
+import { InboxOutlined } from '@ant-design/icons';
+import ConstValue from "@/utils/value";
+import Http from "@/utils/http";
+const { Dragger } = Upload;
 
 export default function CardOpportunities(props) {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
     const handleOk = () => {
         setIsModalOpen(false);
     };
@@ -12,10 +18,59 @@ export default function CardOpportunities(props) {
         setIsModalOpen(false);
     };
     const { data } = props;
+    const propsD = {
+        name: 'file',
+        multiple: true,
+        action: '/files',
+        accept: 'jpg, .jpeg, .png,.pdf,.doc,.docx,',
+        onChange(info) {
+            messageApi.open({
+                key: 'updatable',
+                type: 'loading',
+                content: 'Loading...',
+            });
+            if (info?.file?.response?.data) {
+                uploadData(info?.file?.response?.data)
+            }
+        },
+        showUploadList: false, maxCount: 1,
+    };
+    const uploadData = async (data2) => {
+        let res = await Http.to.items("biographical_notes").createOne({
+
+            biographical: data2?.id,
+            position: {
+                "create": [
+                    {
+                        "biographical_notes_id": "+",
+                        "collection": "recruit",
+                        "item": {
+                            "id": data.id
+                        }
+                    }
+                ],
+                "update": [],
+                "delete": []
+            }
+        });
+        if (res) {
+            setIsModalOpen(false);
+            messageApi.open({
+                key: 'updatable',
+                type: 'success',
+                content: 'success!',
+                duration: 2,
+            })
+        }
+    }
     return (
         <div className='card_opportunities'>
+            {contextHolder}
             <div className='upload_svg'>
-                <div className='name'>Apply</div>
+                <Upload {...propsD} >
+                    <div className='name' >Apply</div>
+                </Upload>
+
             </div>
             <span className='read_more' onClick={() => { setIsModalOpen(true) }}>READ MORE</span>
             <div>
@@ -71,11 +126,47 @@ export default function CardOpportunities(props) {
                 </div>
 
                 <div>
-                    <div className='upload_svg'>
-                        <div className='name'>Apply <div className='svg'></div></div>
-                    </div>
+                    <Row>
+                        <Col sm={24} xl={12}>
+                            <div className='upload_box'>
+
+
+                                <Upload {...propsD} >
+                                    <div className='upload_svg' onClick={() => setShowModal(true)}>
+                                        <div className='name'>Apply <div className='svg'></div></div>
+                                    </div>
+                                </Upload>
+
+                            </div>
+
+
+
+                        </Col>
+                        <Col sm={24} xl={12}>
+                            <div className='upload_box'>
+                                <div className='upload_svg'>
+                                    <a className='name' href={data.link}  target="_blank">Linkedin <div className='svg2'></div></a>
+                                </div>
+                            </div>
+
+                        </Col>
+                    </Row>
+
+
                 </div>
-            </Modal>
-        </div>
+            </Modal >
+            {/* <Modal open={showModal}  onCancel={()=>{setShowModal(false)}} footer={[]} width={720}>
+                <Dragger {...propsD}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">
+                        Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                        banned files.
+                    </p>
+                </Dragger>
+            </Modal> */}
+        </div >
     )
 }
