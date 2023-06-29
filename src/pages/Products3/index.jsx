a141b985fabcfdbba8737a3e35f60fae9eb265a3
张继芮 <1528879339@qq.com>
2023-06-29 13:12:23 +0800

    bug

--- src/pages/Products3/index.jsx
+++ src/pages/Products3/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import './index.scss'
 import img_bg from '@/static/img/bg_3.jpg'
-import { Carousel, Row, Col, Table, message } from 'antd'
+import { Carousel, Row, Col, Table, message,Button } from 'antd'
 import NavLink from '@/components/NavLink'
 import { useParams } from 'react-router-dom';
 import Http from "@/utils/http";
@@ -102,11 +102,14 @@ export default function Products3() {
             key: 'operation',
             fixed: 'right',
             width: 59,
-            render: (e, t, index) => { return (e.part_no && (!flag[index]) && ((!flag2[index])?(<div className='download'><a onClick={(item) => { download(e, 'data', index) }} ><CloudDownloadOutlined /></a> </div>):<div className='svg_load'></div>)) }
+            render: (e, t, index) => { return (e.part_no && (!flag[index]) ? ((!flag2[index])?(<div className='download'><a onClick={(item) => { download(e, 'data', index) }} ><CloudDownloadOutlined /></a> </div>):<div className='svg_load'></div>):<div className='download'><Button  disabled type="link"><CloudDownloadOutlined style={{fontSize:'20px'}}/></Button></div> ) }
         },
     ];
     async function download(e = {}, fileName = 'data', index) {
         let url = '';
+        let obj = { ...flag2 };
+            obj[index] = true
+            setflag2(obj);
         let res = await get(`files?limit=25&fields[]=id&fields[]=modified_on&fields[]=type&fields[]=title&fields[]=type&fields[]=filesize&sort[]=-uploaded_on&page=1&filter=%7B%22_and%22:[%7B%22_and%22:[%7B%22filename_download%22:%7B%22_contains%22:"${e.part_no}"%7D%7D]%7D,%7B%22_and%22:[%7B%22type%22:%7B%22_nnull%22:true%7D%7D,%7B%22folder%22:%7B%22_eq%22:%22579b7761-8fcb-455f-96c7-8112e22d8aab%22%7D%7D]%7D]%7D&meta[]=filter_count&meta[]=total_count`)
         if (!(res?.data?.[0]?.id)) {
             let obj = { ...flag };
@@ -124,6 +127,10 @@ export default function Products3() {
             return
         };
         res?.data.forEach(item => {
+            console.log(item);
+            console.log(item);
+            console.log(item);
+
             let url = './assets/' + item?.id;
             // const a = document.createElement('a');
             // a.style.display = 'none';
@@ -135,9 +142,7 @@ export default function Products3() {
             // a.click();
             // console.dir(a);
             // document.body.removeChild(a);
-            let obj = { ...flag2 };
-            obj[index] = true
-            setflag2(obj);
+            
             var xhr = new XMLHttpRequest();
             xhr.open('GET', url, true); // 也可用POST方式
             xhr.responseType = "blob";
@@ -146,8 +151,7 @@ export default function Products3() {
                     var blob = this.response;
                     if (navigator.msSaveBlob == null) {
                         var a = document.createElement('a');
-                        var headerName = xhr.getResponseHeader("Content-disposition");
-                        var fileName = decodeURIComponent(headerName).substring(20);
+                        var fileName = item.title
                         a.download = fileName;
                         a.href = URL.createObjectURL(blob);
                         document.body.appendChild(a);
